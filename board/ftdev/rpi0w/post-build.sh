afdf34eeb9f8fe73e5807bd01c8f4e7877de72b2
Fernando Caletti <fe.caletti@gmail.com>
2026-09-13 18:25:23 -0300

    0.1.1 Added USB support on boot

--- board/ftdev/rpi0w/post-build.sh
+++ board/ftdev/rpi0w/post-build.sh
@@ -1,1 +1,13 @@
 #!/bin/sh
+set -e
+TARGET_DIR=$1
+
+# Restore /sbin/init to BusyBox init so /etc/inittab is executed
+rm -f "${TARGET_DIR}/sbin/init"
+ln -sf ../bin/busybox "${TARGET_DIR}/sbin/init"
+
+# Ensure test_booted is available in /usr/bin for testing
+if [ -f board/ftdev/rpi0w/rootfs_overlay/sbin/init ]; then
+    mkdir -p "${TARGET_DIR}/usr/bin"
+    cp -f board/ftdev/rpi0w/rootfs_overlay/sbin/init "${TARGET_DIR}/usr/bin/test_booted"
+fi
